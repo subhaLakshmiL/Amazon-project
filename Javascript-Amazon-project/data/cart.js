9ea5c0509ecf5fced381789fa9da9ed7c6f26c6d
Subhalakshmi <lsubha3103@gmail.com>
2024-07-03 12:09:55 +0530

    saved cart in local storage

--- Javascript-Amazon-project/data/cart.js
+++ Javascript-Amazon-project/data/cart.js
@@ -1,10 +1,20 @@
-export let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-},{
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if(!cart){
+    cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+    },{
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+    }];
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
+
 
 export function addToCart(productId, quantity) {
     let matchingItem = cart.find(cartItem => cartItem.productId === productId);
@@ -17,6 +27,8 @@ export function addToCart(productId, quantity) {
             quantity: quantity
         });
     }
+
+    saveToStorage();
 }
 
 //1) create a new array  2) loop through the cart  3) add each product to new array, except for this productId
@@ -29,4 +41,6 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
